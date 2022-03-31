d97328e21d5fca62fa507a6a27c4272a57b32596
MirkOrlando <mirko@orlando-m.it>
2022-03-31 14:41:43 +0200

    created jsnack4 while loop

---
+++ jsnack4/main.js
@@ -0,0 +1,26 @@
+/* Snack4
+Crea un array di numeri interi e 
+fai la somma di tutti gli elementi 
+che sono in posizione dispari
+*/
+
+const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+sum = 0;
+
+let i = 0;
+while (i < numbersList.length) {
+  numb = numbersList[i];
+  //console.log(`${i}: ${numb}`);
+  switch (i % 2) {
+    case 1:
+      sum += numb;
+      break;
+
+    default:
+      break;
+  }
+
+  i++;
+}
+
+console.log(sum);
