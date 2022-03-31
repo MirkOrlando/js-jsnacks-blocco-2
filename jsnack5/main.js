652ca436f7f4cbc83d2c72db1853ad1cb1d6b8de
MirkOrlando <mirko@orlando-m.it>
2022-03-31 15:23:27 +0200

    added jsnack5 for loop solution

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -7,9 +7,16 @@ fino a quando ne avrà tanti quanti l’altro.
 const array1 = ["lamponi", "latte", "pasta", "olio", "sale", "libro"];
 const array2 = ["uova", "insalata", "saponetta"];
 
+console.log(array2.length, array1.length);
+
 while (array2.length < array1.length) {
   const newElement = "elementoExtra";
   array2.push(newElement);
 }
+
+/* for (let i = array2.length; i < array1.length; i = array2.length) {
+  const newElement = "elementoExtra";
+  array2.push(newElement);
+} */
 
 console.log(array2.length, array1.length);
