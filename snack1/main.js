89a515c016a4d88448b319ec9696d52d0ea6808a
MirkOrlando <mirko@orlando-m.it>
2022-03-31 12:47:27 +0200

    created snack1

---
+++ snack1/main.js
@@ -0,0 +1,32 @@
+/* SNACK 1 
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Esegui questo programma in due versioni, con il for e con il while.
+ */
+
+/* programma con ciclo for */
+/* const numbersList = [];
+let sum = 0;
+
+for (let i = 0; i < 5; i++) {
+  userNumber = prompt("inserisci un numero");
+  numbersList.push(userNumber);
+  numb = parseInt(numbersList[i]);
+  sum += numb;
+  console.log(numbersList[i]);
+}
+
+console.log(sum); */
+
+/* programma con ciclo while */
+let sum = 0;
+let i = 0;
+
+while (i < 5) {
+  userNumber = parseInt(prompt("inserisci un numero"));
+  sum += userNumber;
+
+  i++;
+}
+
+console.log(sum);
