f235057e7b152f015cd30a17b0a5cf9cbeb2b6ec
MirkOrlando <mirko@orlando-m.it>
2022-04-01 10:28:38 +0200

    added correction jsnack5

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -4,14 +4,24 @@ Aggiungi elementi all’array che ha meno elementi
 fino a quando ne avrà tanti quanti l’altro.
 */
 
+/* creare due array con numeri casuali */
 const array1 = ["lamponi", "latte", "pasta", "olio", "sale", "libro"];
 const array2 = ["uova", "insalata", "saponetta"];
 
 console.log(array2.length, array1.length);
 
-while (array2.length < array1.length) {
-  const newElement = "elementoExtra";
-  array2.push(newElement);
+if (array1.length < array2.length) {
+  while (array1.length < array2.length) {
+    const newElement = "elementoExtra";
+    array1.push(newElement);
+  }
+} else if (array2.length < array1.length) {
+  while (array2.length < array1.length) {
+    const newElement = "elementoExtra";
+    array2.push(newElement);
+  }
+} else {
+  console.log("i due array hanno già la stessa lunghezza");
 }
 
 /* for (let i = array2.length; i < array1.length; i = array2.length) {
